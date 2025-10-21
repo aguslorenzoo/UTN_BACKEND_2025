411341173ced2bb0bc57a677e67ba5261bc90492
aguslorenzoo <agustina.lorenzo@gmail.com>
2025-10-21 16:20:30 -0300

    .

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,72 +1,68 @@
 import User from "../models/User.model.js"
 
 class UserRepository {
-    static async create(name, email, password){
-        try{
-            return await User.create({
+
+    static async create(name, email, password) {
+        try {
+            return await User.insertOne({
                 name: name,
                 email: email,
                 password: password
             })
-            console.log('[SERVER]: usuario creado exitosamente')
         }
-        catch(error){
-            console.log('[SERVER ERROR]: no se pudo crear el usuario', error)
-        }
-    }
-
-    static async getAll(){
-        try{
-            const users = await User.find()
-            console.log(users)
-            return users
-        }
-        catch{
-            console.log('[SERVER ERROR]: no se pudo obetener la lista', error)
-        }
-    }
-
-    static async getById(user_id){
-        try{
-            const user_found = await User.findById(user_id)
-            return user_found
-        }
-        catch{
-            console.log('[SERVER ERROR]: no se pudo obetener el usuario con id ' + user_id, error)
+        catch (error) {
+            console.error('[SERVER ERROR]: no se pudo crear el usuario', error)
             throw error
         }
     }
 
-    static async getByEmail(email){
+    static async getAll() {
         try{
-            const user_found = await User.findOne({email: email})
-            console.log(user_found)
+            const users = await User.find(
+                {
+                    active: true
+                }
+            )
+            return users
+        }
+        catch(error){
+            console.error('[SERVER ERROR]: no se pudo obtener la lista de usuarios', error)
+            throw error
+        }
+    }
+
+    static async getById(user_id) {
+        try{    
+            const user_found = await User.findById(user_id)
             return user_found
         }
         catch(error){
-            console.log('[SERVER ERROR]: no se pudo obetener el usuario con email ' + email, error)
+            console.error('[SERVER ERROR]: no se pudo obtener el usuario con id ' + user_id, error)
+            throw error
         }
     }
 
-    static async deleteById(user_id){
-        try{
-            const response = await User.findByIdAndDelete(user_id)
-            console.log(response)
-            return response
-        }
-        catch(error){
-            console.log('[SERVER ERROR]: no se pudo obetener el id para elimianrlo ' + user_id, error)
-        }
+    static async getByEmail (email){
+        const user_found = await User.findOne({
+            email: email, 
+            active: true
+        })
+        return user_found
     }
 
-    static async updateById(user_id, update_user){
-        try{
-            await User.findByIdAndUpdate(user_id, update_user)
-        }
-        catch(error){
-            console.log('[SERVER ERROR]: no se pudo obetener el id para actualizar el usuario ' + user_id, error)
-        }
+    static async deleteById (user_id){
+        const response = await User.findByIdAndDelete(user_id)
+        return response
+    }
+
+    static async updateById (user_id, update_user){
+        console.log(user_id, update_user)
+        await User.findByIdAndUpdate(
+            user_id,
+            update_user
+        )
     }
 }
+
 
 export default UserRepository
